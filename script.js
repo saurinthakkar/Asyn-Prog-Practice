1181d1f83f1cecb79c6651d6bbad7292ed8570ae
Saurin Thakkar <saurin.thakkar13@gmail.com>
2022-02-18 23:54:45 +0530

    practiced some examples of js concepts

---
+++ script.js
@@ -0,0 +1,63 @@
+import { addition } from "/mymodules/addition.js";
+console.log(addition(9, 8));
+// const url = "https://gbfs.citibikenyc.com/gbfs/en/station_information.json";
+// let fetchRes = fetch(url);
+
+// fetchRes
+//   .then((res) => res.json())
+//   .then((d) => {
+//     console.log(d);
+//   });
+//Block scope
+// function addtn() {
+//   var x = 2;
+//   console.log(x);
+// }
+// addtn();
+// console.log(x);
+
+//Simple function and function expression
+
+// function simple() {
+//   return 2 + 2;
+// }
+
+// console.log(simple());
+
+// const square = function fact(number) {
+//   return number * number;
+// };
+
+// console.log(square(5));
+
+// let person = {
+//   firstName: "John",
+//   lastName: "Doe",
+//   age: 70,
+//   eyecolor: "blue",
+//   "prop-2": "three",
+//   3: "three",
+// };
+
+// console.log(person["prop-2"], person["age"]);
+
+// const hero = {
+//   name: "batman",
+// };
+
+// const { name } = hero;
+// console.log(name);
+
+//Timeout question
+
+// let timeout;
+
+// function myTimeout() {
+//   timeout = setTimeout(alertFunc, 3000);
+// }
+
+// function alertFunc() {
+//   alert("hello");
+// }
+
+// myTimeout();
